3acf3c84941d2f0f1d2be83e4627acfaf119ae89
ChiragDate <cdate022001@gmail.com>
2025-05-02 17:27:27 +0000

    update to localhost

--- src/service.py
+++ src/service.py
@@ -61,4 +61,4 @@ def predict(input_data: PredictionInput):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="127.0.0.1", port=8000)
+    uvicorn.run(app, host="localhost", port=8000)
