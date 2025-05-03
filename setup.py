0a6b349c0e2496e5c33f4042d6f234986f80bda8
ChiragDate <cdate022001@gmail.com>
2025-05-03 13:41:23 +0000

    code in src folder of setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import find_packages, setup
 
 setup(
     name='src',
-    packages=find_packages(),
     version='0.1',
     description='A short description of the project.',
     author='chirag mohit',
     license='',
-    packages=find_packages(),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
     install_requires=[],
 )
