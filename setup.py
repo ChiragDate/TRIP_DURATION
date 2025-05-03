602970793e7c5abf216a258c91e4b92b6c30afe7
ChiragDate <cdate022001@gmail.com>
2025-05-03 13:36:50 +0000

    Updated setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,10 @@ from setuptools import find_packages, setup
 setup(
     name='src',
     packages=find_packages(),
-    version='0.1.0',
+    version='0.1',
     description='A short description of the project.',
     author='chirag mohit',
     license='',
+    packages=find_packages(),
+    install_requires=[],
 )
